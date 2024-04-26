88de90b75a83c1dcf6e4427fa824970e580a7894
minas mike <menelikmichael@gmail.com>
2024-04-26 17:10:07 +0300

    air pollution component finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-import Temprature from "./api/temprature/Temprature";
+import Temprature from "./components/temprature/Temprature";
 import Navbar from "./components/Navbar";
+import AirPollution from "./components/AirPollution/AirPollution";
 
 export default function Home() {
   return (
     <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
-       <Navbar />
-       <div className="pb-4 flex flex-col gap-4 md:flex-row">
+      <Navbar />
+      <div className="pb-4 flex flex-col gap-4 md:flex-row">
         <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]"> <Temprature /></div>
-        <div className="flex flex-col"></div>
-       </div>
+
+        <div className="flex flex-col w-full">
+          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+            <AirPollution />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
